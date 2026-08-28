2d01a654fd6c47f3d00895d1be331a1e86319b19
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 00:44:33 +0300

    Auto update 2026-08-29 00:44:33

--- src/main/java/net/mcreator/kaizokuocraft/client/CombatVanillaHud.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/CombatVanillaHud.java
@@ -1,4 +1,25 @@
 package net.mcreator.kaizokuocraft.client;
 
-public class CombatVanillaHud {
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.RenderGuiLayerEvent;
+import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
+
+public final class CombatVanillaHud {
+
+    private CombatVanillaHud() {
+    }
+
+    @SubscribeEvent
+    public static void hideVanillaHotbar(
+            RenderGuiLayerEvent.Pre event
+    ) {
+
+        if (!CombatState.isActive()) {
+            return;
+        }
+
+        if (event.getName().equals(VanillaGuiLayers.HOTBAR)) {
+            event.setCanceled(true);
+        }
+    }
 }
