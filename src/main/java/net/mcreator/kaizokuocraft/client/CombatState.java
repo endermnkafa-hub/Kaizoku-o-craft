b1240c38245f69ca4c1ced1c9330c9792951f3aa
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 00:42:46 +0300

    Auto update 2026-08-29 00:42:46

--- src/main/java/net/mcreator/kaizokuocraft/client/CombatState.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/CombatState.java
@@ -1,4 +1,21 @@
 package net.mcreator.kaizokuocraft.client;
 
-public class CombatState {
+public final class CombatState {
+
+    private static boolean active = false;
+
+    private CombatState() {
+    }
+
+    public static boolean isActive() {
+        return active;
+    }
+
+    public static void setActive(boolean value) {
+        active = value;
+    }
+
+    public static void toggle() {
+        active = !active;
+    }
 }
