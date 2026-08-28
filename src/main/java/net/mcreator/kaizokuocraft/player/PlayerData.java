40e4f5bc90336e211b0ff94c28cdef02bbb2ec8b
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-28 23:14:00 +0300

    Auto update 2026-08-28 23:14:00

--- src/main/java/net/mcreator/kaizokuocraft/player/PlayerData.java
+++ src/main/java/net/mcreator/kaizokuocraft/player/PlayerData.java
@@ -1,4 +1,48 @@
 package net.mcreator.kaizokuocraft.player;
 
-public class PlayerData {
+import net.minecraft.nbt.CompoundTag;
+import net.neoforged.neoforge.common.util.INBTSerializable;
+
+public class PlayerData implements INBTSerializable<CompoundTag> {
+
+    private int level = 1;
+    private long experience = 0L;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = Math.max(1, level);
+    }
+
+    public long getExperience() {
+        return experience;
+    }
+
+    public void setExperience(long experience) {
+        this.experience = Math.max(0L, experience);
+    }
+
+    public void addExperience(long amount) {
+        if (amount > 0) {
+            experience += amount;
+        }
+    }
+
+    @Override
+    public CompoundTag serializeNBT() {
+        CompoundTag tag = new CompoundTag();
+
+        tag.putInt("Level", level);
+        tag.putLong("Experience", experience);
+
+        return tag;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundTag tag) {
+        level = Math.max(1, tag.getInt("Level"));
+        experience = Math.max(0L, tag.getLong("Experience"));
+    }
 }
