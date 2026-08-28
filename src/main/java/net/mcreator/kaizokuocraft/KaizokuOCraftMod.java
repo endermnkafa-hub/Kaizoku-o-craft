5e2a13f1f47d73adce7be113b48dcf00d24e02e6
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 00:53:25 +0300

    Auto update 2026-08-29 00:53:25

--- src/main/java/net/mcreator/kaizokuocraft/KaizokuOCraftMod.java
+++ src/main/java/net/mcreator/kaizokuocraft/KaizokuOCraftMod.java
@@ -13,7 +13,7 @@ import net.neoforged.fml.util.thread.SidedThreadGroups;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.api.distmarker.Dist;
+
 
 import net.minecraft.server.TickTask;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
@@ -23,7 +23,8 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.mcreator.kaizokuocraft.player.*;
 import net.mcreator.kaizokuocraft.network.SyncPlayerDataPacket;
 import net.mcreator.kaizokuocraft.client.KaizokuHud;
-import net.mcreator.kaizokuocraft.client.ClientInit;
+import net.neoforged.fml.Dist;
+import net.neoforged.fml.DistExecutor;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.Queue;
@@ -42,7 +43,11 @@ public class KaizokuOCraftMod {
 
 	public KaizokuOCraftMod(IEventBus modEventBus) {
 		// Start of user code block mod constructor
-		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientInit::init);
+		DistExecutor.safeRunWhenOn(
+	        Dist.CLIENT,
+	        () -> ClientInit::init
+	    );
+
 		NeoForge.EVENT_BUS.register(KaizokuHud.class);
 		ModAttachments.register(modEventBus);
 		NeoForge.EVENT_BUS.register(PlayerDataEvents.class);
