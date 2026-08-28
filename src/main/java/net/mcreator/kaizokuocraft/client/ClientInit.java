52d5c06770e1e768b73e9a350970d614dc8be275
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 00:45:08 +0300

    Auto update 2026-08-29 00:45:08

--- src/main/java/net/mcreator/kaizokuocraft/client/ClientInit.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/ClientInit.java
@@ -1,4 +1,28 @@
 package net.mcreator.kaizokuocraft.client;
 
-public class ClientInit {
+import net.neoforged.bus.api.IEventBus;
+
+public final class ClientInit {
+
+    private ClientInit() {
+    }
+
+    public static void init(IEventBus modEventBus) {
+
+        modEventBus.addListener(
+                CombatKeyMappings::register
+        );
+
+        modEventBus.register(
+                CombatClientEvents.class
+        );
+
+        modEventBus.register(
+                CombatHud.class
+        );
+
+        modEventBus.register(
+                CombatVanillaHud.class
+        );
+    }
 }
