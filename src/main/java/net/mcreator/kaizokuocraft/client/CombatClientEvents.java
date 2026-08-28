eb2ee69c889751ff737b1df75b080a6000d64fba
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 00:43:34 +0300

    Auto update 2026-08-29 00:43:34

--- src/main/java/net/mcreator/kaizokuocraft/client/CombatClientEvents.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/CombatClientEvents.java
@@ -1,4 +1,27 @@
 package net.mcreator.kaizokuocraft.client;
 
-public class CombatClientEvents {
+import net.minecraft.client.Minecraft;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+
+public final class CombatClientEvents {
+
+    private CombatClientEvents() {
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+
+        while (CombatKeyMappings.TOGGLE_COMBAT.consumeClick()) {
+
+            Minecraft minecraft = Minecraft.getInstance();
+
+            if (minecraft.player == null) {
+                return;
+            }
+
+            CombatState.toggle();
+        }
+    }
 }
