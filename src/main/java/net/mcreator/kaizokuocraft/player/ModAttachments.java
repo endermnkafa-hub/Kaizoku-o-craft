e62297e377d1039f3a2f28379b9f2af70d9da289
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-28 23:14:35 +0300

    Auto update 2026-08-28 23:14:35

--- src/main/java/net/mcreator/kaizokuocraft/player/ModAttachments.java
+++ src/main/java/net/mcreator/kaizokuocraft/player/ModAttachments.java
@@ -1,4 +1,31 @@
 package net.mcreator.kaizokuocraft.player;
 
-public class ModAttachments {
+import java.util.function.Supplier;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import net.mcreator.kaizokuocraft.KaizokuOCraftMod;
+
+public final class ModAttachments {
+
+    public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES =
+            DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, KaizokuOCraftMod.MODID);
+
+    public static final Supplier<AttachmentType<PlayerData>> PLAYER_DATA =
+            ATTACHMENT_TYPES.register(
+                    "player_data",
+                    () -> AttachmentType.serializable(PlayerData::new)
+                            .copyOnDeath()
+                            .build()
+            );
+
+    private ModAttachments() {
+    }
+
+    public static void register(IEventBus modEventBus) {
+        ATTACHMENT_TYPES.register(modEventBus);
+    }
 }
