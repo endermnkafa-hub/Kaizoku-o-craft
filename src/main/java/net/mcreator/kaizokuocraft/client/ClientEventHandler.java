8cf858194d75bee8c13c7baac88d6f2ce193027e
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 00:58:46 +0300

    Auto update 2026-08-29 00:58:46

--- src/main/java/net/mcreator/kaizokuocraft/client/ClientEventHandler.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/ClientEventHandler.java
@@ -1,4 +1,24 @@
 package net.mcreator.kaizokuocraft.client;
 
-public class ClientEventHandler {
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+
+import net.minecraft.client.Minecraft;
+
+@EventBusSubscriber(
+        modid = "kaizoku_o_craft",
+        bus = EventBusSubscriber.Bus.MOD,
+        value = net.neoforged.api.distmarker.Dist.CLIENT
+)
+public final class ClientEventHandler {
+
+    private ClientEventHandler() {
+    }
+
+    @SubscribeEvent
+    public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
+        event.register(CombatKeyMappings.TOGGLE_COMBAT);
+    }
 }
