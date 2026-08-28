8bc2060c89c06ad48920bd8e6e00ecaf6b0da132
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 00:54:10 +0300

    Auto update 2026-08-29 00:54:10

--- src/main/java/net/mcreator/kaizokuocraft/client/CombatKeyMappings.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/CombatKeyMappings.java
@@ -1,30 +1,35 @@
 package net.mcreator.kaizokuocraft.client;
 
 import com.mojang.blaze3d.platform.InputConstants;
-
 import net.minecraft.client.KeyMapping;
-
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
 
 import org.lwjgl.glfw.GLFW;
 
 public final class CombatKeyMappings {
-
-    public static final String CATEGORY =
-            "key.categories.kaizoku_o_craft";
 
     public static final KeyMapping TOGGLE_COMBAT =
             new KeyMapping(
                     "key.kaizoku_o_craft.toggle_combat",
                     InputConstants.Type.KEYSYM,
                     GLFW.GLFW_KEY_K,
-                    CATEGORY
+                    "key.categories.kaizoku_o_craft"
             );
 
     private CombatKeyMappings() {
     }
 
-    public static void register(RegisterKeyMappingsEvent event) {
+    @SubscribeEvent
+    public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
         event.register(TOGGLE_COMBAT);
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+        while (TOGGLE_COMBAT.consumeClick()) {
+            CombatState.toggle();
+        }
     }
 }
