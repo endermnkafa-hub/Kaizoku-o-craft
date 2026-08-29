1242920f8969425f78c475f374091e4f00a32235
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 12:19:08 +0300

    Auto update 2026-08-29 12:19:08

--- src/main/java/net/mcreator/kaizokuocraft/client/PlayerPowerClient.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/PlayerPowerClient.java
@@ -1,4 +1,17 @@
 package net.mcreator.kaizokuocraft.client;
 
-public class PlayerPowerClient {
+public final class PlayerPowerClient {
+
+    private PlayerPowerClient() {
+    }
+
+    public static double getDamageMultiplier(
+            long level
+    ) {
+        if (level < 1L) {
+            level = 1L;
+        }
+
+        return Math.sqrt(level);
+    }
 }
