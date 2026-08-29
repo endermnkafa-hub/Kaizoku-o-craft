57e27f412aaf6b2d541d56a3daf119d1536b8521
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 11:35:20 +0300

    Auto update 2026-08-29 11:35:20

--- src/main/java/net/mcreator/kaizokuocraft/client/ClientEventHandler.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/ClientEventHandler.java
@@ -1,7 +1,6 @@
 package net.mcreator.kaizokuocraft.client;
 
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 
 public final class ClientEventHandler {
@@ -16,16 +15,5 @@ public final class ClientEventHandler {
         event.register(
                 CombatKeyMappings.TOGGLE_COMBAT
         );
-    }
-
-    @SubscribeEvent
-    public static void onClientTick(
-            ClientTickEvent.Post event
-    ) {
-        while (
-                CombatKeyMappings.TOGGLE_COMBAT.consumeClick()
-        ) {
-            CombatState.toggle();
-        }
     }
 }
