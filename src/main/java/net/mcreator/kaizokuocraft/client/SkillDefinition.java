c666e023add88bdc17f3518a5fc082225d16c20b
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 12:49:14 +0300

    Auto update 2026-08-29 12:49:14

--- src/main/java/net/mcreator/kaizokuocraft/client/SkillDefinition.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/SkillDefinition.java
@@ -5,6 +5,11 @@ import net.minecraft.world.item.ItemStack;
 public record SkillDefinition(
         String id,
         String name,
-        ItemStack icon
+        ItemStack icon,
+        long cooldownMillis
 ) {
+
+    public double getCooldownSeconds() {
+        return cooldownMillis / 1000.0D;
+    }
 }
