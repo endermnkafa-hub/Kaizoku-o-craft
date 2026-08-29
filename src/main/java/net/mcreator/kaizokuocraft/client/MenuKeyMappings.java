de46c8247f0863cbb068b24107af41624eb19851
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-29 12:15:22 +0300

    Auto update 2026-08-29 12:15:22

--- src/main/java/net/mcreator/kaizokuocraft/client/MenuKeyMappings.java
+++ src/main/java/net/mcreator/kaizokuocraft/client/MenuKeyMappings.java
@@ -1,4 +1,21 @@
 package net.mcreator.kaizokuocraft.client;
 
-public class MenuKeyMappings {
+import com.mojang.blaze3d.platform.InputConstants;
+
+import net.minecraft.client.KeyMapping;
+
+import org.lwjgl.glfw.GLFW;
+
+public final class MenuKeyMappings {
+
+    public static final KeyMapping OPEN_MENU =
+            new KeyMapping(
+                    "key.kaizoku_o_craft.open_menu",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_M,
+                    "key.categories.kaizoku_o_craft"
+            );
+
+    private MenuKeyMappings() {
+    }
 }
